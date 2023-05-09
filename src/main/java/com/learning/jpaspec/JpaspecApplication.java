eae781ae4fc38cdefaf7ee003eae5130d0c144fa
Doofensmith <wahyuwijanarko3998@gmail.com>
2023-05-09 10:01:11 +0700

    Add Springboot project

---
+++ src/main/java/com/learning/jpaspec/JpaspecApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jpaspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaspecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/jpaspec/JpaspecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jpaspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaspecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
